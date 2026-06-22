00df4d7b8f911f7ab3c1a3e63cad2769a2e17b02
shivteja1597 <tejashiv123@gmail.com>
2026-06-22 23:24:13 +0530

    chore: clean up temporary python test scripts and debug artifacts

--- test_duckdb5.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-
-con = duckdb.connect()
-try:
-    con.execute("ATTACH 'warehouse' AS iceberg_lake (TYPE iceberg, ENDPOINT 'http://nessie:19120/iceberg', AUTHORIZATION_TYPE 'none');")
-    res = con.execute("SHOW TABLES FROM iceberg_lake.lake_curated;").fetchall()
-    print("TABLES IN lake_curated:", res)
-except Exception as e:
-    print("ERROR:", e)
